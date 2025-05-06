d14752773c631d0f3277418285211e9af46d4cb5
FatihNitro <FatihNitro@DESKTOP-KERR77G>
2025-05-06 14:41:34 +0300

    Add project files.

---
+++ Helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mehmet Fatih ÜNAL Patika ile birlikte yazılım dünyasına adım atmaktayım. Başarılı bir eğitim sonucunda yazılım sektöründe profesyonel bir full stack developer olmayı hedefliyorum.");
+        }
+    }
+}
